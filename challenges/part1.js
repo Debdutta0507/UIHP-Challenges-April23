57f4b07e7acf36dead9372c1e5cb95e250ed0a55
Debdutta Chatterjee <debduttachatterjee@Debduttas-MacBook-Air.local>
2025-05-24 22:28:48 +0530

    adding commit

--- challenges/part1.js
+++ challenges/part1.js
@@ -1,1 +1,10 @@
 /** Write your code below */
+let post=""
+jsInput=document.querySelector('input')
+jsDiv=document.querySelector('div')
+let handelInput=()=>{
+    post=jsInput.value
+    jsDiv.textContent=post
+}
+jsInput.addEventListener('input',handelInput)
+
--- challenges/part2.js
+++ challenges/part2.js
@@ -1,11 +1,21 @@
-let post = '';
+let post = undefined;
 
-const jsInput = document.querySelector('input');
-const jsDiv = document.querySelector('div');
-
-function handleInput() {
-  post = jsInput.value;
-  jsDiv.textContent = post;
+const jsInput = document.querySelector("input");
+const jsDiv = document.querySelector("div");
+function dataToView() {
+  jsInput.value = post===undefined?"What's on your mind?":post;
+  jsDiv.textContent = post===undefined?"":post;
 }
 
+function handleInput() {
+  post=jsInput.value
+  dataToView();
+}
+function handleClick() {
+  post = "";
+  dataToView();
+}
+dataToView()
+
+jsInput.onclick = handleClick;
 jsInput.oninput = handleInput;
